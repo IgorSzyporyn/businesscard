4bd6d1dcfd2bda7560509aa686b785f4b09b0b48
IgorSzyporyn <ij@woba.io>
2022-02-27 09:26:53 +0100

    fix: updated current workplace

--- src/card.ts
+++ src/card.ts
@@ -1,49 +1,55 @@
 #!/usr/bin/env node
 
-import chalk from 'chalk'
-import boxen, { BorderStyle } from 'boxen'
+import chalk from "chalk";
+import boxen, { BorderStyle } from "boxen";
 
 const options: boxen.Options = {
   padding: 1,
   margin: 1,
   borderStyle: BorderStyle.Round,
-}
+};
 
 const data = {
-  name: chalk.white('Igor Szyporyn Jørgensen'),
-  handle: chalk.cyan('IgorSzyporyn'),
-  title: chalk.white('Lead Frontend Developer'),
-  workplace: chalk.white('Appstract'),
-  twitter: chalk.cyan('https://twitter.com/IgorSzyporyn'),
-  github: chalk.cyan('https://github.com/IgorSzyporyn'),
-  linkedin: chalk.cyan('https://linkedin.com/in/IgorSzyporyn'),
-  npm: chalk.cyan('https://npmjs.org/package/igor-szyporyn'),
-  npx: chalk.white('npx isj'),
-  labelTwitter: chalk.white.bold('    Twitter:'),
-  labelGitHub: chalk.white.bold('     GitHub:'),
-  labelNpm: chalk.white.bold('        NPM:'),
-  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
-  labelNpmPackages: chalk.white.bold('  NPM Packages:'),
-  npmStorybookFacelift: chalk.cyan('https://npmjs.org/package/storybook-facelift'),
-  npmReactTimeoutButton: chalk.cyan('                 https://npmjs.org/package/react-timeout-button'),
-  npmEnvOverlay: chalk.cyan('                 https://npmjs.org/package/env-overlay'),
-}
+  name: chalk.white("Igor Szyporyn Jørgensen"),
+  handle: chalk.cyan("IgorSzyporyn"),
+  title: chalk.white("Lead Frontend Developer"),
+  workplace: chalk.white("Woba"),
+  twitter: chalk.cyan("https://twitter.com/IgorSzyporyn"),
+  github: chalk.cyan("https://github.com/IgorSzyporyn"),
+  linkedin: chalk.cyan("https://linkedin.com/in/IgorSzyporyn"),
+  npm: chalk.cyan("https://npmjs.org/package/igor-szyporyn"),
+  npx: chalk.white("npx isj"),
+  labelTwitter: chalk.white.bold("    Twitter:"),
+  labelGitHub: chalk.white.bold("     GitHub:"),
+  labelNpm: chalk.white.bold("        NPM:"),
+  labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+  labelNpmPackages: chalk.white.bold("  NPM Packages:"),
+  npmStorybookFacelift: chalk.cyan(
+    "https://npmjs.org/package/storybook-facelift"
+  ),
+  npmReactTimeoutButton: chalk.cyan(
+    "                 https://npmjs.org/package/react-timeout-button"
+  ),
+  npmEnvOverlay: chalk.cyan(
+    "                 https://npmjs.org/package/env-overlay"
+  ),
+};
 
-const newline = '\n'
-const heading = `${data.name}`
-const title = `${data.title}`
-const workplace = `${data.workplace}`
-const twittering = `${data.labelTwitter}  ${data.twitter}`
-const npming = `${data.labelNpm}  ${data.npm}`
-const githubing = `${data.labelGitHub}  ${data.github}`
-const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`
+const newline = "\n";
+const heading = `${data.name}`;
+const title = `${data.title}`;
+const workplace = `${data.workplace}`;
+const twittering = `${data.labelTwitter}  ${data.twitter}`;
+const npming = `${data.labelNpm}  ${data.npm}`;
+const githubing = `${data.labelGitHub}  ${data.github}`;
+const linkedining = `${data.labelLinkedIn}  ${data.linkedin}`;
 
 const output =
   heading +
   newline +
   title +
   newline +
-  workplace + 
+  workplace +
   newline +
   newline +
   newline +
@@ -53,6 +59,6 @@ const output =
   newline +
   linkedining +
   newline +
-  twittering 
+  twittering;
 
-console.log(chalk.green(boxen(output, options)))
+console.log(chalk.green(boxen(output, options)));
